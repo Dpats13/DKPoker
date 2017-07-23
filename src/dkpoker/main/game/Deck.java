9ff73b87a5373cd962c7dc147ff5b8684c5904a0
Dpats13 <Drp5291@psu.edu>
2017-07-22 23:34:37 -0400

    commit

--- src/dkpoker/main/game/Deck.java
+++ src/dkpoker/main/game/Deck.java
@@ -1,5 +1,51 @@
 package dkpoker.main.game;
-
 public class Deck {
-
+	
+	private Card[] deck;
+	private String[] values; 
+	
+	public Deck() {
+		deck = new Card[52];
+		values = new String[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+		generateDiamonds();
+		generateHearts();
+		generateClubs();
+		generateSpaids();
+	}
+	
+	private void generateDiamonds() {
+		int count = 0;
+		for (int i = 0; i < 13; i++) {
+			deck[i] = new Card(values[count], "Diamonds", "Deck");
+			count++;
+		}
+	}
+	
+	private void generateHearts() {
+		int count = 0;
+		for (int i = 13; i < 26; i++) {
+			deck[i] = new Card(values[count], "Hearts", "Deck");
+			count++;
+		}
+	}
+	
+	private void generateClubs() {
+		int count = 0;
+		for (int i = 26; i < 39; i++) {
+			deck[i] = new Card(values[count], "Clubs", "Deck");
+			count++;
+		}
+	}
+	
+	private void generateSpaids() {
+		int count = 0;
+		for (int i = 39; i < 52; i++) {
+			deck[i] = new Card(values[count], "Spaids", "Deck");
+			count++;
+		}
+	}
+	
+	public Card[] getDeck() {
+		return deck;
+	}
 }
--- src/dkpoker/main/game/Main.java
+++ src/dkpoker/main/game/Main.java
@@ -3,7 +3,11 @@ package dkpoker.main.game;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Deck deck = new Deck();
+		for (int i = 0; i < 52; i++) {
+			System.out.println(deck.getDeck()[i].toString());
+		}
 
 	}
 
