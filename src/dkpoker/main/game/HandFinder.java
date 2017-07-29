d5bf0816e48941860f6ecaee0d8950806009b96d
Kyle Eckenstine <Kyle@Kyles-MacBook-Pro.local>
2017-07-29 12:36:31 -0400

    did you commit?

--- src/dkpoker/main/game/HandFinder.java
+++ src/dkpoker/main/game/HandFinder.java
@@ -6,90 +6,102 @@ public class HandFinder {
 
 	}
 
-	public void findHandRank(Hand hand){
+	public Card[] findHandRank(Hand hand){
 		Card[] bestHand = new Card[5];
 		
-		if(checkRoyalFlush(hand) != null){
+		if((bestHand = checkRoyalFlush(hand)) != null){
 			hand.setHandRank(9);
-		}else if(checkStraightFlush(hand) != null){
+		}else if((bestHand = checkStraightFlush(hand)) != null){
 			hand.setHandRank(8);
-		}else if(checkFourOfAKind(hand) != null){
+		}else if((bestHand = checkFourOfAKind(hand)) != null){
 			hand.setHandRank(7);
-		}else if(checkFullHouse(hand) != null){
+		}else if((bestHand = checkFullHouse(hand)) != null){
 			hand.setHandRank(6);
-		}else if(checkFlush(hand) != null){
+		}else if((bestHand = checkFlush(hand)) != null){
 			hand.setHandRank(5);
-		}else if(checkStraight(hand) != null){
+		}else if((bestHand = checkStraight(hand)) != null){
 			hand.setHandRank(4);
-		}else if(checkThreeOfAKind(hand) != null){
+		}else if((bestHand = checkThreeOfAKind(hand)) != null){
 			hand.setHandRank(3);
-		}else if(checkTwoPair(hand) != null){
+		}else if((bestHand = checkTwoPair(hand)) != null){
 			hand.setHandRank(2);
-		}else if(checkPair(hand) != null){
+		}else if((bestHand = checkPair(hand)) != null){
 			hand.setHandRank(1);
 		}else{
+			bestHand = checkHighCard(hand);
 			hand.setHandRank(0);
 		}
 		
+		return bestHand;
 	}
 	
 
 	public Card[] checkRoyalFlush(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkStraightFlush(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkFourOfAKind(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkFullHouse(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkFlush(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkStraight(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkThreeOfAKind(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkTwoPair(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkPair(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
 
 	public Card[] checkHighCard(Hand hand) {
 		Card cards[] = new Card[5];
+		Card playerHand[] = hand.getPlayerHand();
 		
 		return cards;
 	}
